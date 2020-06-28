80faa0520a60f3327ec18e3b3923249af7b9642e
Liliya Verchenko <ip_verchenko@inbox.ru>
2020-06-28 17:16:50 +0300

    Changed cook_book5

--- cook_book5.py
+++ cook_book5.py
@@ -1,21 +1,22 @@
-def create_dict_from_file(file_name):
+from pprint import pprint
+
+def create_dict_from_file(file):
     cook_dict = {}
     with open ('recipes.txt', encoding='utf-8') as f:
         for line in f:
-            dish_name = f.readline()
-            print(line.lower().strip())
+            dish_name = line.lower().strip()
             counter = int(f.readline())
             list_of_ingredient = [] # <временный список>
             for i in range(counter):
                 temp_dict = {} #- временный словарь
-                print(f.readline().lower())
-                ingredients_list = (f.readline().rstrip().replace('|', '').split())
+                ingredients_list = f.readline().strip().split('|')
                 #ingredient = f.readline() # вот так перемещаемся по файлу
-                temp_dict['ingredient_name'] = ingredients_list[0] # <заполняем словарь с ингридиетом>
-                temp_dict['quantity'] = ingredients_list[1]
-                temp_dict['measure'] = ingredients_list[2]
+                temp_dict['ingredient_name'] = ingredients_list[0].strip() # <заполняем словарь с ингридиетом>
+                temp_dict['quantity'] = ingredients_list[1].strip()
+                temp_dict['measure'] = ingredients_list[2].strip()
                 list_of_ingredient.append(temp_dict) #<добавляем словарь в список>
             cook_dict[dish_name] = list_of_ingredient
             f.readline() #- еще раз смещаетмся т.к. там пустая срока
+    pprint(cook_dict)
     return cook_dict
 create_dict_from_file('recipes.txt')
